f143d9bef7d4bbd3aead820ee9c8f1b3eedc5514
Tyler Long <tyler.stephen.long@gmail.com>
2015-02-08 15:23:51 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mergicduhrgathering::Application.config.secret_key_base = 'c717d29b6bb96b32d2187af9e12e42ba8b24d34a4865558042e99158e0ad16dd96133dbc048c2be011bfad41389ab25f09f586f63dbfac86e03a8999ca62d30e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mergicduhrgathering::Application.config.session_store :cookie_store, key: '_mergicduhrgathering_session'
